36f35264dc9450a7262f6d040c3d521d46db1b95
Tolben <vitek.pasek@gmail.com>
2025-11-20 22:18:42 +0100

    regen schema

--- schema/030_foreign_keys.postgres.sql
+++ schema/030_foreign_keys.postgres.sql
@@ -1,4 +1,4 @@
--- Auto-generated from schema-map-postgres.psd1 (map@9d3471b)
+-- Auto-generated from schema-map-postgres.psd1 (map@62c9c93)
 -- engine: postgres
 -- table:  policy_kms_keys
 ALTER TABLE policy_kms_keys ADD CONSTRAINT fk_policy_kms_keys_policy FOREIGN KEY (policy_id) REFERENCES encryption_policies(id) ON DELETE CASCADE;
